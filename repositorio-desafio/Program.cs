e375e92ac912d20adc155fad3ad9c520c3ff33e2
André Nunes de Lima <andre.nunes@live.com>
2022-05-26 01:53:46 -0300

    Second commit - adding a new line

--- repositorio-desafio/Program.cs
+++ repositorio-desafio/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("This project is for a creation of a test repository on GitHub");
+Console.WriteLine("New line to generate a new commit");
